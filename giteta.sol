7d88d07673de48047444fadb4ea6a1516e6b66da
David Kamer <me@davidkamer.com>
2023-06-03 12:34:27 -0400

    commit hash contract skeleton

---
+++ giteta.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity >= "0.8.18";
+
+contract giteta {
+  // TODO - make address argument for relays
+  address gitarg = 0xb3Bdb3e25BB580CA98480a5fD7c98E6516750685;
+  constructor() {
+    
+  }
+}
