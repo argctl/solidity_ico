97d9c3718df005fec11bf17fd1dc74a41cbf860d
David Kamer <me@davidkamer.com>
2023-06-06 11:33:36 -0400

    theoretical import

--- giteta.sol
+++ giteta.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity >= "0.8.18";
+import "./gitarg.sol";
 
 contract giteta {
   // TODO - make address argument for relays
@@ -46,6 +47,6 @@ contract giteta {
   }
   function repo(address wallet, string memory name, string memory url) public returns (uint) {
     require(!repoLock[wallet]);
-    addressRepo[wallet] = Repo(name, url, wallet)
+    addressRepo[wallet] = Repo(name, url, wallet);
   }
 }
