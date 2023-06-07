a52a19c7eb97405eac286bee2dc5bc2e22cbc2a9
David Kamer <me@davidkamer.com>
2023-06-07 12:29:23 -0400

    gitorg direct

--- libraries/gitorg.sol
+++ libraries/gitorg.sol
@@ -1,4 +1,29 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >= "0.8.18";
 
-library gitorg {}
+//library so that there is a single point and interface to externals reference
+library gitorg {
+  // TODO - library interface exteranal to gitarg
+  // uses contract addresses to pool/poll versioning data 
+  // "directs traffic"
+  function donate(address giteta, address gitarg) public returns (uint) {
+    return 0;
+  }
+  function direct(address giteta, address gitarg, uint stash) public returns (uint) {
+    require(stash >= msg.value); // REVIEW - query for rate on contract? 
+    // TODO - giteta gitarg bridge
+    return 0;
+  }
+  function stats() public returns (uint) {
+    return 0;
+  }
+  function pool() public returns (uint) {
+    return 0;
+  }
+  function poll() public returns (uint) {
+    return 0;
+  }
+  function search() public returns (address) {
+    return msg.sender;
+  }
+}
