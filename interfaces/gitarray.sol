8b2aa8cc69c8cd7431ad0e149dbe24b8bc9e5be2
David Kamer <me@davidkamer.com>
2023-06-11 15:51:13 -0400

    function names

--- interfaces/gitarray.sol
+++ interfaces/gitarray.sol
@@ -5,5 +5,8 @@ interface gitarray {
   function burn(address wallet, uint commits) external returns (uint);
   function hash(address wallet, string memory hash, string memory message,
     string memory commitHash, uint clientTimestamp, uint blockTimestamp) external returns (address);
+  function eta() external returns (uint); //TODO
+  function org() external returns (uint); //TODO
+  function arg() external returns (uint); //TODO
   //TODO
 }
