1ed82aa12eeb17e1d3f39a8f61c635daeb275cb7
David Kamer <me@davidkamer.com>
2023-06-11 15:05:06 -0400

    gitarray hash interface

--- interfaces/gitarray.sol
+++ interfaces/gitarray.sol
@@ -2,6 +2,8 @@
 pragma solidity >= "0.8.18";
 
 interface gitarray {  
-  function burn(address wallet, uint commits) returns (uint);
+  function burn(address wallet, uint commits) external returns (uint);
+  function hash(address wallet, string memory hash, string memory message,
+    string memory commitHash, uint clientTimestamp, uint blockTimestamp) external returns (address);
   //TODO
 }
