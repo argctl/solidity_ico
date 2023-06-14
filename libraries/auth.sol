5110a5355ad7630d0da9ff6e20e40ee0bea5f961
David Kamer <me@davidkamer.com>
2023-06-14 13:52:05 -0400

    results by wallet

--- libraries/auth.sol
+++ libraries/auth.sol
@@ -16,6 +16,14 @@ library auth {
     actions.push(log);
     return actions;
   }
+  function query(Log[] storage actions, Log[] storage result) private returns (Log[] storage) {
+    for (uint i = 0; i < actions.length; i++) {
+      if (actions[i].wallet == msg.sender) {
+        result.push(actions[i]);
+      }
+    }
+    return result;
+  }
   // result should be empty pointer to Log storage array
   function query(address _contract, Log[] storage actions, Log[] storage result) private returns (Log[] storage) {
     for (uint i = 0; i < actions.length; i++) {
