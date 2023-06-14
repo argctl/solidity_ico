7fa2d9b780f3c402c23b0cfbf4732c64bfda287b
David Kamer <me@davidkamer.com>
2023-06-13 20:46:11 -0400

    auth action function

--- libraries/auth.sol
+++ libraries/auth.sol
@@ -8,5 +8,12 @@ library auth {
     uint timestamp;
     address _contract;
     string _type;
+    string _action;
+  }
+  function action (address _contract, string calldata _type,
+    string calldata _action, Log[] storage actions) internal returns (Log[] storage) {
+    Log memory log = Log(msg.sender, block.timestamp, _contract, _type, _action);
+    actions.push(log);
+    return actions;
   }
 }
