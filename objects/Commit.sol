6992bdfa631c1a1bfda72e374a0950c868b72bb9
David Kamer <me@davidkamer.com>
2023-06-25 17:50:56 -0400

    permanent code hash

--- objects/Commit.sol
+++ objects/Commit.sol
@@ -7,12 +7,21 @@ contract Commit {
   bytes commit;
   bytes author;
   bytes date;
+  bytes hash;
+  uint hashTime;
   constructor(address _wallet, address _repo, bytes memory _commit, bytes memory _author, bytes memory _date) {
     wallet = _wallet;
     repo = _repo;
     commit = _commit;
     author = _author;
     date = _date;
+  }
+
+  function setCodeHash(bytes memory _hash) public returns (uint) {
+    require(hash.length == 0);
+    hash = _hash;
+    hashTime = block.timestamp;
+    return hashTime;
   }
 
 }
