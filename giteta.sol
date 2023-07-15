8f04baaf4ad3f4857fd6efc9cbeb739e5aa9e907
David Kamer <me@davidkamer.com>
2023-07-15 15:14:48 -0400

    base functions to match white paper

--- giteta.sol
+++ giteta.sol
@@ -9,7 +9,7 @@ import "./objects/Log.sol";
 
 contract giteta {
   address gitargWallet;
-  address gitarg;
+  gitarg Gitarg;
 
   struct Time {
     address commit;
@@ -18,57 +18,73 @@ contract giteta {
 
   mapping(Repo => Time[]) commits;
   mapping(address => Time) valuing;
+  mapping(address => uint) bounties;
   mapping(address => uint) values;
   
   constructor(address _gitarg) {
-    gitarg = _gitarg;
+    Gitarg = gitarg(_gitarg);
     gitargWallet = msg.sender;
   }
   // REVIEW - payinto repo?
   //constructor(address _wallet, address _repo, bytes memory _message, bytes memory _author, bytes memory _date) {
-  function commit(address repo, string memory message, string memory author, string memory date) public returns (uint) {
-    Commit c = new Commit(msg.sender, repo, message, author, date);
-    Repo repo = Repo(repo);
+  function commit(address _repo, string memory message, string memory author, string memory date) public returns (uint) {
+    Commit c = new Commit(msg.sender, _repo, message, author, date);
+    Repo repo = Repo(_repo);
     Time memory time = Time(address(c), block.timestamp);
     commits[repo].push(time);
     valuing[address(c)] = time;
     return block.timestamp;
   }
   // raise value of commits - called when used successfully by chain
-  function up(address commit) public returns (uint) { //re-up?
+  function up(address payable _commit) public payable returns (uint) { //re-up?
     // subtract timestamps 
     // TODO - repo owner or gitarray tie in?
-    Time memory time = valuing[commit];
-    uint value = block.timestamp - time.timestamp;
-    values[commit] = value;
-    return value;
+    Time memory time = valuing[_commit];
+    uint _value = block.timestamp - time.timestamp;
+    // REVIEW - gte
+    require(Gitarg.balanceOf(msg.sender) > _value);
+    // REVIEW - repo instead of commit?
+    Gitarg.transferFrom(msg.sender, _commit, _value);
+    values[_commit] = _value;
+    return _value;
   }
   // raise value of commits - called when issue is created involving commit
-  function down(address commit) public returns (uint) {
+  function down(address payable _repo, address payable _commit, uint bounty) public payable returns (uint) {
+    // TODO - check gitarg token stash of wallet
+    Repo repo = Repo(_repo);
+    // TODO - gitorg library to use handshakes
+    // REVIEW - should this be the commit owner?
+    require(msg.sender == repo.getOwner());
+    require(values[_commit] >= bounty && Gitarg.balanceOf(_commit) >= bounty);
+    Gitarg.transferFrom(_commit, _repo, bounty);
+    // REVIEW - bounty needed
+    bounties[_commit] += bounty;
+    // REVIEW - values based on the git token backed?
+    values[_commit] -= bounty;
     return 0;
   }
   // query value of commit
-  function value(address commit) public returns (uint) {
-    return values[commit];
+  function value(address _commit) public view returns (uint) {
+    return values[_commit];
   }
   // query commits by range, repo or value
-  function query(address repo, uint start, uint end) public returns (Time[] memory) {
+  function query(address _repo, uint start, uint end) public view returns (Time[] memory) {
     Time[] memory _commits;
-    Time[] memory repo = commits[Repo(repo)];
+    Time[] memory repo = commits[Repo(_repo)];
     for (uint i = 0; i < repo.length; i++) {
       Time memory time = repo[i];
       if (time.timestamp >= start && time.timestamp <= end) _commits[i] = time;
     }
     return _commits; 
   }
-  function query(address repo) public returns (Time[] memory) {
+  function query(address repo) public view returns (Time[] memory) {
     return commits[Repo(repo)];
   }
-  function query(address _repo, uint value) public returns (Time[] memory) {
+  function query(address _repo, uint _value) public view returns (Time[] memory) {
     Time[] memory _commits;
     Time[] memory repo = commits[Repo(_repo)];
     for (uint i = 0; i < repo.length; i++) {
-      if (values[repo[i].commit] == value) {
+      if (values[repo[i].commit] == _value) {
         _commits[i] = repo[i];
       }
     }
