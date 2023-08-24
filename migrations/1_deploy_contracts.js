99f8c0f3d562ac7fc6a92b179638ceaed9e1eaa9
David Kamer <me@davidkamer.com>
2023-08-24 10:52:04 -0400

    migration for repo

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,9 +1,14 @@
 //const Linkerfinder = artifacts.require('Linkerfinder')
 const gitarg = artifacts.require('gitarg')
+const giteta = artifacts.require('giteta')
+const Repo = artifacts.require('Repo')
 
 module.exports = function (deployer, network, accounts){
   //deployer.deploy(Ad, accounts[0], "https://linkerfinder.com", false, 10000)
   //deployer.deploy(Linkerfinder, accounts, 10000, 3)
   deployer.deploy(gitarg)
+  deployer.deploy(giteta, accounts[0])
+  //constructor(string memory _name, string memory _url, address _owner) payable {
+  deployer.deploy(Repo, 'TestRepo', 'https://gitlab.com/me2211/testrepo', accounts[1])
 } 
 
