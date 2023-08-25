73f041a18829c3a3eb80f6bef2ddb7002d946c40
David Kamer <me@davidkamer.com>
2023-08-25 19:35:30 -0400

    update gitarray and repo stamping

--- contracts/gitarray.sol
+++ contracts/gitarray.sol
@@ -5,105 +5,9 @@ import "./giteta.sol";
 import "./gitarg.sol";
 import "./objects/Repo.sol";
 import "./objects/Commit.sol";
+import "./objects/Proposal.sol";
+import "./objects/Handshakes.sol";
 
-contract Proposal {
-  uint private proposal;
-  string private value;
-  address private object;
-  address private creator;
-  bool public safeMode = true;
-
-  modifier safe () {
-    if (safeMode) require(msg.sender == creator);
-    _;
-  }
-
-  constructor(uint _proposal, string memory _value, address _object) {
-    creator = msg.sender;  
-    object = _object;
-    value = _value;
-    proposal = _proposal;
-  }
-  function disableSafeMode() public safe() returns(uint)  {
-    require(msg.sender == creator);
-    safeMode = false;
-    return block.timestamp;
-  }
-  function getProposal() public view safe() returns(uint) {
-    if (safeMode) require(msg.sender == creator);
-    return proposal;
-  }
-  function getValue() public view safe() returns(string memory) {
-    if (safeMode) require(msg.sender == creator);
-    return value;
-  }
-  function getObject() public view safe() returns(address) {
-    if (safeMode) require(msg.sender == creator);
-    return object;
-  }
-  function getCreator() public view safe() returns(address) {
-    if (safeMode) require(msg.sender == creator);
-    return creator;
-  }
-}
-
-contract Handshakes {
-  address creator;
-  address owner;
-  mapping(address => uint) handshakes;
-  mapping(address => uint) private shook;
-  uint threshold;
-
-  address proposal;
-  uint proposalTime;
-
-  bool corp;
-
-  modifier owned () {
-    if (corp) require(msg.sender == owner);
-  }
-
-  constructor(address[] memory _handshakes, address _owner, uint _threshold, bool corp) {
-    creator = msg.sender;
-    owner = _owner;
-    proposal = proposal;
-    if (threshold == 0) {
-      threshold = _handshakes.length;
-    } else {
-      threshold = _threshold;
-    }
-    for (uint i = 0; i < _handshakes.length; i++) {
-      handshakes[_handshakes[i]] = block.timestamp;
-    }
-  }
-  function setProposal(address _proposal) public returns(uint) {
-    proposal = _proposal;
-    proposalTime = block.timestamp;
-    return block.timestamp;
-  }
-  function isHandshake(address sender) public view returns(bool) {
-    // review - owner and creator or just owner
-    require(creator == msg.sender || owner == msg.sender);
-    return handshakes[sender] != 0;
-  }
-  function isHandshake() public view returns(bool) {
-    return handshakes[msg.sender] != 0;
-  }
-  function shake() public {
-    shook[msg.sender] = block.timestamp;
-  }
-  // TODO - expiry on handshake check with greaterthan
-  function check(address[] memory shakes, address[] memory noshakes) public owned {
-    for (uint i = 0; i < shakes.length; i++) {
-      require(handshakes[shakes[i]] != 0);
-      require(shook[shakes[i]] != 0);
-    }
-    for (uint i = 0; i < noshakes.length; i++) {
-      require(handshakes[noshakes[i]] != 0);
-      require(shook[noshakes[i]] == 0);
-    }
-  }
-}
 
 contract gitarray {
 
@@ -112,19 +16,27 @@ contract gitarray {
   uint threshold;
   mapping(address => Handshakes) signers;
   mapping(address => Repo) repos;
-  
-  constructor(address[] memory _handshakes, address _owner) {
+  address public arg;
+  // !IMPORTANT
+  // !!!
+  // REVIEW - threshold divider 
+  constructor(address[] memory _handshakes, address _owner, address _gitarg) {
+    arg = _gitarg;
     threshold = _handshakes.length;
     creator = msg.sender;
     owner = _owner;
-    Handshakes handshakes = new Handshakes(_handshakes, _owner, threshold);
+    //constructor(address[] memory _handshakes, address _owner, uint _threshold, bool _corp) {
+    // REVIEW - corp var
+    Handshakes handshakes = new Handshakes(_handshakes, _owner, threshold, true);
     signers[msg.sender] = handshakes;
   }  
   // REVIEW - customizable threshold?
   function repo(address[] memory _handshakes, string memory _name, string memory _url, address _owner) public returns(uint) {
-    Handshakes handshakes = new Handshakes(_handshakes, _owner, threshold);
+    // REVIEW - corp var
+    Handshakes handshakes = new Handshakes(_handshakes, _owner, threshold, true);
     signers[msg.sender] = handshakes;
-    Repo _repo = new Repo(_name, _url, _owner);
+    //constructor(string memory _name, string memory _url, address _owner, address _gitarg, address _gitarray) payable {
+    Repo _repo = new Repo(_name, _url, _owner, arg, address(this));
     repos[msg.sender] = _repo;
     // REVIEW - return repo object or address?
     return block.timestamp;
@@ -150,8 +62,6 @@ contract gitarray {
   // TODO - add handshake
   //function handshake
   // TODO - remove handshake
-  //function handshake
-  // TODO - total recursive signers
   //function total
   // TODO - action with totaled handshakes
   //function action
--- contracts/objects/Repo.sol
+++ contracts/objects/Repo.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-// TODO - change to unlicense (unlinsenced?) 
+// REVIEW - change to unlicense (unlinsenced?) 
 pragma solidity >= "0.8.18";
 
 import "./Commit.sol";
@@ -9,7 +9,9 @@ contract Repo {
   string public name;
   string public url;
   address public owner;
-  address gitArrayInstance;
+  bytes32[] private hash;
+  address array;
+  address arg;
   mapping(address => uint) public buyerContributions;
   address[] buyers;
   uint public value;
@@ -17,7 +19,11 @@ contract Repo {
   mapping (address => Commit) contributorCommits;
 
   // v
-  address creator;
+  address private creator; 
+  bool private flag;
+  mapping(address => uint) public allowed;
+  // workers rights
+  mapping(address => uint) private revoked;
 
   modifier seller (address payable _seller) {
     require(msg.sender == owner && _seller == owner, "owner-seller auth failure");
@@ -27,35 +33,65 @@ contract Repo {
     require(msg.sender == owner);
     _;
   }
-
   modifier created () {
     require(msg.sender == creator);
     _;
   }
-  
+  function allow (address _allow) public owned  {
+    allowed[_allow] = block.timestamp;
+  }
+  function revoke (address _disallow, bool clean) public owned {
+    if (clean) allowed[_disallow] = 1; // 1 makes them still appear on the list if queried directly for the name
+    revoked[_disallow] = block.timestamp;
+  }
+  constructor(string memory _name, string memory _url, address _owner, address _gitarg, address _gitarray) payable {
+    if(_gitarray != msg.sender) flag = true;
+    creator = msg.sender;
+    name = _name;
+    url = _url;
+    owner = _owner;
+    array = msg.sender;
+    arg = _gitarg;
+    value = msg.value;
+    // TODO - gitarray compare for url
+    hash.push(keccak256(abi.encodePacked(url, block.timestamp, msg.sender)));
+  }
+  // REVIEW - should the stamp check for gitarray
+  function stamp(string memory _hash) public {
+    hash.push(keccak256(abi.encodePacked(_hash, block.timestamp, msg.sender)));
+  }
+  // defaults to 0
+  function verification(uint iteration) public view created returns (bytes32) {
+    // REVIEW - can call gitarg directly?
+    return hash[iteration];
+  }
+  // verify with gitarray address also verified
+  function verification(address payable _gitarray, uint iteration) public view returns (bytes32) {
+    require(_gitarray == creator, "verification address is not creator");
+    // REVIEW - can call gitarg directly?
+    return hash[iteration];
+
+  }
   function commit(Commit _commit, address sender) public returns(uint) {
     commits.push(_commit);//i
     contributorCommits[sender] = _commit;
     return block.timestamp;
   }
   
-  constructor(string memory _name, string memory _url, address _owner) payable {
-    creator = msg.sender;
-    name = _name;
-    url = _url;
-    owner = _owner;
-    gitArrayInstance = msg.sender;
-    value = msg.value;
-  }
-  // TODO
-  function sell(address _owner, address payable _seller, uint _value) public seller(_seller) {
+  function _sell(address _owner, address payable _seller, uint _value) internal {
     require(_value <= value, "value transfer is less than amount in contract");
-    //require(_seller == owner, "owner is the seller");
-    //require(msg.sender == owner, "owner is sender");
-    uint diff = value - _value;
-    value = diff;
+    // REVIEW - ledger should be enough - gitarg integration may change this in layer 4-5
+    for (uint i = 0; i < buyers.length; i++) {
+      buyerContributions[buyers[i]] = 0;
+    }
+    delete buyers;
+    value -= _value;
     _seller.transfer(_value);
     owner = _owner;
+  }
+  // TODO - ?
+  function sell(address _owner, address payable _seller, uint _value) public seller(_seller) {
+    _sell(_owner, _seller, _value);
   }
   // if it is a bid change from += to = for [msg.sender]
   function add() payable public {
@@ -69,19 +105,48 @@ contract Repo {
       uint amount = buyerContributions[buyers[i]];
       if (_value == amount) {
         require(buyers[i] == _owner);
-        this.sell(_owner, _seller, _value);
+        _sell(_owner, _seller, _value);
         break;
       }
     }   
   }
-  // sell by highest value
-  function highSell(address _owner, address payable _seller) public seller(_seller) {
-    uint _value;
+  // REVIEW - query functions in different standard?
+  // REVIEW - private?
+  function high() public view returns (uint) {
+    uint _amount;
     for(uint i = 0; i < buyers.length; i++) {
       uint amount = buyerContributions[buyers[i]];
-      if (value > _value) _value = amount;
+      if (amount >= _amount) {
+        _amount = amount;
+      }
+    }
+    return _amount;
+  }
+  // REVIEW - ethics
+  function high(address payable _seller) public view seller(_seller) returns (address) {
+    uint _amount;
+    address _owner;
+    for(uint i = 0; i < buyers.length; i++) {
+      uint amount = buyerContributions[buyers[i]];
+      if (_amount < amount) {
+        _amount = amount;
+        _owner = buyers[i];
+      }
+    }
+    return _owner;
+  }
+  // sell by highest value
+  function highSell(address payable _seller) public seller(_seller) {
+    uint _amount;
+    address _owner;
+    for(uint i = 0; i < buyers.length; i++) {
+      uint amount = buyerContributions[buyers[i]];
+      if (_amount < amount) {
+        _amount = amount;
+        _owner = buyers[i];
+      }
     }
     // we don't care about the bid, just the highest value
-    this.sell(_owner, _seller, _value);
+    _sell(_owner, _seller, _amount);
   }
 }
