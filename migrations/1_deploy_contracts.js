341ce5350237d486fbcf1a1a82b148419db70162
David Kamer <me@davidkamer.com>
2023-08-30 18:15:57 -0400

    migration for gitorg

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -4,6 +4,7 @@ const giteta = artifacts.require('giteta')
 const Repo = artifacts.require('Repo')
 const Handshakes = artifacts.require('Handshakes')
 const gitarray = artifacts.require('gitarray')
+const gitorg = artifacts.require('gitorg')
 
 const { wait } = require('../test/utils')
 
@@ -11,6 +12,7 @@ module.exports = async function (deployer, network, accounts){
   //deployer.deploy(Ad, accounts[0], "https://linkerfinder.com", false, 10000)
   //deployer.deploy(Linkerfinder, accounts, 10000, 3)
   //console.log({ deployer })
+  await deployer.deploy(gitorg)
   await deployer.deploy(gitarg)
   await wait(4000)
   const arg = await gitarg.deployed()//.then(async arg => {
@@ -19,10 +21,13 @@ module.exports = async function (deployer, network, accounts){
   //constructor(address[] memory _handshakes, address _owner, uint _threshold, bool _corp) {
   await deployer.deploy(Handshakes, accounts.slice(1), accounts[0], 3, true)
   //constructor(address[] memory _handshakes, address _owner, address _gitarg) {
+  await deployer.link(gitorg, gitarray)
   await deployer.deploy(gitarray, accounts.slice(1), accounts[0], arg.address)
   const array = await gitarray.deployed()
   //constructor(string memory _name, string memory _url, address _owner, address _gitarg, address _gitarray) payable {
+  await deployer.link(gitorg, Repo)
   await deployer.deploy(Repo, 'TestRepo', 'https://gitlab.com/me2211/testrepo', accounts[1], arg.address, array.address)
+
   //})
   //const arg = await gitarg.deployed()
 } 
