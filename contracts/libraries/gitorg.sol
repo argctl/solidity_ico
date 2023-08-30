bf1990cf28e8aa78a1a67d576e4a339d942ad6a8
David Kamer <me@davidkamer.com>
2023-08-30 17:55:41 -0400

    library move and gitorg stamp func

---
+++ contracts/libraries/gitorg.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >= "0.8.18";
+import "../giteta.sol";
+import "../gitarg.sol";
+
+//library so that there is a single point and interface to externals reference
+
+library gitorg {
+  // TODO - library interface exteranal to gitarg
+  // uses contract addresses to pool/poll versioning data 
+  // "directs traffic"
+  function stamp (string memory _hash, uint timestamp, address _msgSender) public returns (bytes32) {
+    return keccak256(abi.encodePacked(_hash, timestamp, _msgSender));
+  }
+  function donate(address _giteta, address _gitarg) public view returns (uint) {
+    return 0;
+  }
+  function direct(address _giteta, address _gitarg, address repo, uint stash, address payable _to) public returns (uint) {
+    // REVIEW - recycle to either contract address or escrow wallet to cycle GITARG coin
+    giteta eta = giteta(_giteta);
+    gitarg arg = gitarg(_gitarg);
+    address wallet = eta.gitargWallet();
+    require(_to == wallet);
+    // TODO - stash from msg.value or account stash?
+    //require(stash >= msg.value * 100000); // REVIEW - query for rate on contract? 
+    // TODO - use gitarray setter value for inflation deflation control
+    arg.transfer(wallet, stash);
+    //eta.transfer(repo);
+    // TODO - giteta gitarg bridge
+    return block.timestamp;
+  }
+  function stats() public returns (uint) {
+    return 0;
+  }
+  function pool() public returns (uint) {
+    return 0;
+  }
+  function poll() public returns (uint) {
+    return 0;
+  }
+  function search() public returns (address) {
+    return msg.sender;
+  }
+}
