12543895f27a7ee84fb418c244d79ce2db755f08
David Kamer <me@davidkamer.com>
2023-08-30 22:12:20 -0400

    private unaccessable vars

--- contracts/objects/Commit.sol
+++ contracts/objects/Commit.sol
@@ -2,15 +2,20 @@
 pragma solidity >= "0.8.18";
 
 contract Commit {
-  address repo;
-  address wallet;
-  string message;
-  string author;
-  string date;
-  bytes hash;
-  uint hashTime;
+  address public repo;
+  // private wallet that can be made public might just need a check
+  // the rest of the variables need access from repo
+  // could put it in a struct
+  address private wallet;
+  string private message;
+  string private author;
+  string private date;
+  bytes private hash;
+  // should the hash time be public?
+  uint public hashTime;
 
   address creator; // likely giteta contract
+  // code cost
   constructor(address _wallet, address _repo, string memory _message, string memory _author, string memory _date) {
     creator = msg.sender;
     wallet = _wallet;
