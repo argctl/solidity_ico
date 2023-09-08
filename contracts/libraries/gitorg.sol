02b2209afb45fbbc20b247e432d0cabf189f02ce
David Kamer <me@davidkamer.com>
2023-09-08 00:09:57 -0400

    direct functions

--- contracts/libraries/gitorg.sol
+++ contracts/libraries/gitorg.sol
@@ -2,9 +2,13 @@
 pragma solidity >= "0.8.18";
 import "../giteta.sol";
 import "../gitarg.sol";
+import "../objects/Repo.sol";
 
 //library so that there is a single point and interface to externals reference
 
+// !!!
+// REVIEW - msg.value may not be available in library functions and might need to be replaced with contract functions
+// REVIEW - msg.value and transfer - if msg.value is not available, transfer/send shouldn't be?
 library gitorg {
   // TODO - library interface exteranal to gitarg
   // uses contract addresses to pool/poll versioning data 
@@ -12,13 +16,22 @@ library gitorg {
   function stamp (string memory _hash, uint timestamp, address _msgSender) public pure returns (bytes32) {
     return keccak256(abi.encodePacked(_hash, timestamp, _msgSender));
   }
-  function donate(address _giteta, address _gitarg) public view returns (uint) {
-    return 0;
+  function donate(address _giteta, address payable _gitarg) public returns (bool) {
+    // REVIEW - should msg.sender to commit instead of eta or value split
+    //giteta eta = giteta(_giteta);
+    gitarg arg = gitarg(_gitarg);
+    arg.transferFrom(msg.sender, _giteta, msg.value);
+    bool success = _gitarg.send(msg.value);
+    if (!success) {
+      payable(msg.sender).transfer(msg.value);
+    }
+    return success;
   }
-  function direct(address _giteta, address _gitarg, address repo, uint stash, address payable _to) public returns (uint) {
+  function direct(address _giteta, address _gitarg, address payable _repo, uint stash, address payable _to) public returns (bool) {
     // REVIEW - recycle to either contract address or escrow wallet to cycle GITARG coin
     giteta eta = giteta(_giteta);
     gitarg arg = gitarg(_gitarg);
+    //Repo repo = Repo(_repo);
     address wallet = eta.gitargWallet();
     require(_to == wallet);
     // TODO - stash from msg.value or account stash?
@@ -27,7 +40,11 @@ library gitorg {
     arg.transfer(wallet, stash);
     //eta.transfer(repo);
     // TODO - giteta gitarg bridge
-    return block.timestamp;
+    bool success = _repo.send(msg.value);
+    if (!success) {
+      payable(msg.sender).transfer(msg.value);
+    }
+    return success;
   }
   function stats() public returns (uint) {
     return 0;
