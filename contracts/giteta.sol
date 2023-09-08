d25126de1fc308bcca95c4c2b058d3b1fe5c631a
David Kamer <me@davidkamer.com>
2023-09-07 23:23:04 -0400

    remove values map

--- contracts/giteta.sol
+++ contracts/giteta.sol
@@ -9,19 +9,25 @@ import "./objects/Log.sol";
 
 contract giteta {
   // REVIEW - wallet changeable with handshakes in gitarray
+  // REVIEW - should we change the modifiers to private
   address public gitargWallet;
   gitarg public Gitarg;
   address public arg;
+
+  uint public service;
 
   struct Time {
     address commit;
     uint timestamp;
   }
 
-  mapping(Repo => Time[]) commits;
-  mapping(address => Time) valuing;
-  mapping(address => uint) bounties;
-  mapping(address => uint) values;
+  mapping(Repo => Time[]) private commits;
+  mapping(address => Time) private valuing;
+  mapping(address => uint) private bounties;
+
+  event Com(address commit);
+
+  event Debug(uint value);
   
   constructor(address _gitarg) {
     arg = _gitarg;
@@ -31,47 +37,65 @@ contract giteta {
   // REVIEW - payinto repo?
   //constructor(address _wallet, address _repo, bytes memory _message, bytes memory _author, bytes memory _date) {
   function commit(address _repo, string memory message, string memory author, string memory date, uint escrow) public returns (uint) {
-    require(Gitarg.balanceOf(msg.sender) >= escrow);
+    require(Gitarg.balanceOf(msg.sender) >= escrow, "not enough escrow");
+
+
     // REVIEW - should the transfer be placed into the repo?
+
     Commit c = new Commit(msg.sender, _repo, message, author, date);
-    Gitarg.transferFrom(msg.sender, address(c), escrow);
+    //🤯
+
+    service += 1;
+    
+    Gitarg.transferFrom(msg.sender, address(c), escrow - 1);
     Repo repo = Repo(_repo);
     Time memory time = Time(address(c), block.timestamp);
+    // REVIEW - should this really be a repo object as a key or the address
     commits[repo].push(time);
     valuing[address(c)] = time;
-    return block.timestamp;
+
+    emit Com(address(c));
+
+    return Gitarg.balanceOf(address(c));
   }
   // raise value of commits - called when used successfully by chain
-  function up(address payable _commit) public payable returns (uint) { //re-up?
+  function up(address payable _commit, bool _balance) public payable returns (uint) { //re-up?
     // subtract timestamps 
     // TODO - repo owner or gitarray tie in?
+    Commit commit = Commit(_commit);
+
     Time memory time = valuing[_commit];
     uint _value = block.timestamp - time.timestamp;
+    if (_balance) _value -= Gitarg.balanceOf(_commit);
     // REVIEW - gte
-    require(Gitarg.balanceOf(msg.sender) > _value);
+
+    require(Gitarg.balanceOf(msg.sender) > _value, "Balance of wallet is less than the difference (_value)");
+    // TODO - allowance
     // REVIEW - repo instead of commit?
+
     Gitarg.transferFrom(msg.sender, _commit, _value);
-    values[_commit] = _value;
+
     return _value;
   }
   // raise value of commits - called when issue is created involving commit
+  // REVIEW - gitarray 
   function down(address payable _repo, address payable _commit, uint bounty) public payable returns (uint) {
     // TODO - check gitarg token stash of wallet
     Repo repo = Repo(_repo);
+    Commit commit = Commit(_commit);
     // TODO - gitorg library to use handshakes
     // REVIEW - should this be the commit owner?
-    require(msg.sender == repo.owner());
-    require(values[_commit] >= bounty && Gitarg.balanceOf(_commit) >= bounty);
+    require(msg.sender == repo.owner(), "repo owner doesn't match msg.sender");
+    require(Gitarg.balanceOf(address(commit)) >= bounty);
     Gitarg.transferFrom(_commit, _repo, bounty);
     // REVIEW - bounty needed
     bounties[_commit] += bounty;
     // REVIEW - values based on the git token backed?
-    values[_commit] -= bounty;
     return 0;
   }
   // query value of commit
   function value(address _commit) public view returns (uint) {
-    return values[_commit];
+    return Gitarg.balanceOf(address(Commit(_commit)));
   }
   // query commits by range, repo or value
   function query(address _repo, uint start, uint end) public view returns (Time[] memory) {
@@ -90,7 +114,7 @@ contract giteta {
     Time[] memory _commits;
     Time[] memory repo = commits[Repo(_repo)];
     for (uint i = 0; i < repo.length; i++) {
-      if (values[repo[i].commit] == _value) {
+      if (Gitarg.balanceOf(repo[i].commit) == _value) {
         _commits[i] = repo[i];
       }
     }
