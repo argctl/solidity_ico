8cb9809664dc1baeeb3e123c407c42a768b7f608
David Kamer <me@davidkamer.com>
2023-09-16 22:25:38 -0400

    unique repos

--- contracts/gitarray.sol
+++ contracts/gitarray.sol
@@ -1,13 +1,11 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity >= "0.8.18";
 import "./giteta.sol";
-//import "./libraries/gitorg.sol";
 import "./gitarg.sol";
 import "./objects/Repo.sol";
-import "./objects/Commit.sol";
 import "./objects/Proposal.sol";
 import "./objects/Handshakes.sol";
-
+import "./libraries/gitorg.sol";
 
 contract gitarray {
 
@@ -16,6 +14,7 @@ contract gitarray {
   uint threshold;
   mapping(address => Handshakes) signers;
   mapping(address => Repo) repos;
+  mapping(bytes32 => uint) unique;
   address public arg;
   // !IMPORTANT
   // !!!
@@ -33,28 +32,26 @@ contract gitarray {
   // REVIEW - customizable threshold?
   function repo(address[] memory _handshakes, string memory _name, string memory _url, address _owner) public returns(uint) {
     // REVIEW - corp var
+    uint _stamp = unique[gitorg.key(_url)];
+    require(_stamp == 0, "repo not unique"); 
     Handshakes handshakes = new Handshakes(_handshakes, _owner, threshold, true);
     signers[msg.sender] = handshakes;
     //constructor(string memory _name, string memory _url, address _owner, address _gitarg, address _gitarray) payable {
     Repo _repo = new Repo(_name, _url, _owner, arg, address(this));
     repos[msg.sender] = _repo;
     // REVIEW - return repo object or address?
+    unique[gitorg.key(_url)] = block.timestamp;
     return block.timestamp;
   }
   // TODO - commit with one or more handshakes
-
-/*
   function commit(address signer, string memory _message, string memory _author, string memory _date) public returns(uint) {
     Handshakes handshakes = signers[signer];
     Repo _repo = repos[signer];
     require(handshakes.isHandshake(msg.sender));
     //constructor(address _wallet, address _repo, string memory _message, string memory _author, string memory _date) {
-    Commit _commit = new Commit(msg.sender, address(_repo), _message, _author, _date);
-    _repo.commit(_commit, msg.sender);
     return block.timestamp;
   }
-
-*/
+  // HERE - function approve
   // REVIEW - commit call total?
   //function commit()
   // REVIEW - in handshakes object
