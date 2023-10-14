e6788c9a0f527df25b14f295d023c50e31c96f0b
David Kamer <me@davidkamer.com>
2023-10-14 05:28:45 -0400

    commit!

--- test/argctl.js
+++ test/argctl.js
@@ -1,10 +1,10 @@
 const argctl = artifacts.require('argctl')
 const repo = artifacts.require('repo')
 
-contract('argctl', () => {
-  //it('can commit', async () => {
-    //const ctl = await argctl.deployed()
-    //const _repo = await repo.deployed()
+contract('argctl', (accounts) => {
+  it('can commit', async () => {
+    const ctl = await argctl.deployed()
+    const _repo = await repo.deployed()
     /*
     commit 84227419a8d956aa5f5ccfd27286705e202243fe
     Author: David Kamer <me@davidkamer.com>
@@ -13,8 +13,8 @@ contract('argctl', () => {
     untyped proposal
     */
     //function commit(address _repo, string memory _message, string memory _author, string memory _date) public returns(uint) {
-    //await ctl.commit(_repo.address, 'untyped proposal', 'David Kamer <me@davidkamer.com>', 'Sun Oct 1 03:40:44 2023 -0400')
-  //})
+    await ctl.commit(_repo.address, 'untyped proposal', 'David Kamer <me@davidkamer.com>', 'Sun Oct 1 03:40:44 2023 -0400', { from: accounts[1] })
+  })
   //function checkin (address repo, address handshakes) public {
   it('checkin times', async () => {
 
