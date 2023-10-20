ba0a7ad6a944deaf58c0e9939fb07e6bf00197a8
David Kamer <me@davidkamer.com>
2023-10-20 05:33:07 -0400

    address type

---
+++ contracts/objects/_address.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >= "0.8.20";
+import "./_type.sol";
+
+contract _address is _type {
+  address private A;
+
+  //event ISITREADYYET(address a);
+
+  constructor (address a) {
+    A = a;
+  }
+  function time () public override OUR {
+    emit dens(abi.encodePacked(A));
+    //emit ISITREADYYET(A);
+  }
+  function address_ () public override OUR returns (address) {
+    return A;
+  }
+}
