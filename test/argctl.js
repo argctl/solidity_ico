14f0ff602095fcc084ece0d2ed226ace554a12a0
David Kamer <me@davidkamer.com>
2023-11-13 00:05:52 -0500

    work towards commit function

--- test/argctl.js
+++ test/argctl.js
@@ -1,13 +1,44 @@
 const argctl = artifacts.require('argctl')
+const gitarray = artifacts.require('gitarray')
 const repo = artifacts.require('repo')
 const Handshakes = artifacts.require('Handshakes')
 
 contract('argctl', (accounts) => {
+  let _repo
   it('can add repo', async () => {
+    //function repo(address[] memory _handshakes, string memory _name, string memory _url, address _argctl) public returns(address) {
+    // TODO -  review get handshakes from handshakes object good or security issue?
+    //function repo(address[] memory _handshakes, string memory _name, string memory _url, address _argctl) public returns(address) {
+    const handshakes = accounts.slice(1)
+    const _handshakes = await Handshakes.deployed()
+    const array = await gitarray.deployed()
+    const ctl = await argctl.deployed()
+    //await _handshakes.add(ctl.address)
+    await _handshakes.add(array.address, { from: accounts[5] })
+    handshakes.push(ctl.address)
+    handshakes.push(array.address)
+    console.log({ handshakes })
+    await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve()
+      }, 5000) 
+    })
+    console.log('ctl: ', ctl.address)
+    console.log({ handshakes })
+    console.log('has value: ', handshakes.includes(accounts[2]))
+    console.log('has value: ', handshakes.includes(ctl.address))
+    _repo = await ctl.repo(handshakes, "gitarg_eth_ico", "gitlab.com:me2211/gitarg_eth_ico.git", ctl.address, { from: accounts[2] })
+    console.log({ _repo })
+    //const array = await gitarray.deployed()
+    //const b = await array.somethingdifferent({ from: accounts[3] })
+    //console.log({ b })
+    //console.log({ _repo, accounts2: accounts[2] })
   })
   it('can commit', async () => {
     const ctl = await argctl.deployed()
-    const _repo = await repo.deployed()
+    const array = await gitarray.deployed()
+    //array.
+    //const _repo = await repo.deployed()
     //const handshakes = await Handshakes.deployed()
     /*
     commit 84227419a8d956aa5f5ccfd27286705e202243fe
@@ -25,7 +56,9 @@ contract('argctl', (accounts) => {
     //const isHandshake_ = await handshakes.isHandshake({ from: accounts[1] })
     //console.log({ isHandshake, isHandshake_ })
     //function commit(address _repo, string memory _message, string memory _author, string memory _date) public returns(uint) {
-    //const commitReceipt = await ctl.commit(_repo.address, 'untyped proposal', 'David Kamer <me@davidkamer.com>', 'Sun Oct 1 03:40:44 2023 -0400', { from: accounts[1] })
+    const repo_ = await array.repo({ from: accounts[2] })
+    console.log({ repo_ })
+    const commitReceipt = await ctl.commit(repo_, 'untyped proposal', 'David Kamer <me@davidkamer.com>', 'Sun Oct 1 03:40:44 2023 -0400', { from: accounts[1] })
     //console.log({ commitReceipt })
 
   })
