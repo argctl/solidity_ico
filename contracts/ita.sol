92b6806dc858a5494b6c36e94c11e10ade7f145a
David Kamer <me@davidkamer.com>
2023-11-14 21:17:49 -0500

    programmer gurantee

--- contracts/ita.sol
+++ contracts/ita.sol
@@ -8,15 +8,18 @@ interface ERC20 {
 
 contract ita {
   mapping(uint => address) private owners;
-  uint public creation;
+  uint private creation;
+  event T(uint s);
   constructor (address[] memory _owners) {
     creation = block.timestamp + _owners.length;
     for (uint i = 0; i < _owners.length; i++) {
       owners[block.timestamp + i] = _owners[i];
     }
+    emit T(block.timestamp);
   }
 
-  function allow (address token, uint amount, uint i) public returns (bool) {
+  function allow (address token, uint amount, uint i, uint mn) public returns (bool) {
+    require((mn + i) == creation, "creator");
     require(owners[creation + i] == msg.sender, "own");
     ERC20 erc20 = ERC20(token);
     return erc20.approve(msg.sender, amount);
