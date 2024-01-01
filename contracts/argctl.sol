6a812e05381691e0fb8f208d8985256bfa38d780
David Kamer <me@gitarg.com>
2024-01-01 10:49:59 +0000

    debug: trace last error from working session

--- contracts/argctl.sol
+++ contracts/argctl.sol
@@ -97,7 +97,7 @@ contract argctl {
     // that the interface issue may be the problem and an issue with ganache or something string related
     uint escrow = 1;
     Gitarg.approve(address(giteta_), escrow);
-    giteta_.argctl(_repo, _message, _author, _date, escrow);
+    //giteta_.argctl(_repo, _message, _author, _date, escrow);
     return block.timestamp;
   }
   function tar (uint amount) public returns (uint price) {
