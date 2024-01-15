2ab06e5f63f7ead605944f16e1563a48a507aaaa
David Kamer <me@gitarg.com>
2024-01-15 12:37:56 +0000

    stable rig

---
+++ contracts/rig.sol
@@ -0,0 +1,22 @@
+pragma solidity >= "0.8.20";
+import "./gitar.sol";
+
+contract rig {
+    constructor (uint buy, uint sell, uint booty) {
+        // TODO - initiate gitar contract
+        // booty is the max take for the stake
+        // aaarrrggg
+        // 
+    }
+    function port (uint amount, uint stiphen) public returns (bool) {
+        require(stiphen <= gasLeft(), "stiphen prediction limiter fail");
+        // left side, sell
+        // cannons return loot
+    }
+
+    function star (uint amount, uint stiphen) public returns (bool) {
+        require(stiphen <= gasLeft(), "stiphen prediction limiter fail");
+        // right side, buy
+        // balls rerun shoot
+    }
+}
