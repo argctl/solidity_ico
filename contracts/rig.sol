fa7db2e3b9961f114c61a9773f2d3dac500a0c0f
David Kamer <me@gitarg.com>
2024-01-15 13:13:16 +0000

    fix returns

--- contracts/rig.sol
+++ contracts/rig.sol
@@ -14,10 +14,12 @@ contract rig {
     function port (uint amount, uint stiphen) public tar(stiphen) returns (bool) {
         // left side, sell
         // cannons return loot
+        return false;
     }
 
     function star (uint amount, uint stiphen) public tar(stiphen) returns (bool) {
         // right side, buy
         // balls rerun shoot
+        return false;
     }
 }
