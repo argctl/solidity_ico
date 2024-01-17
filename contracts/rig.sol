98a27e004ffd8653d54ef97a98cef6751b2cb29a
David Kamer <me@gitarg.com>
2024-01-17 19:55:10 +0000

    add payable modifier

--- contracts/rig.sol
+++ contracts/rig.sol
@@ -15,7 +15,7 @@ contract rig {
         require(stiphon < gasleft(), "stiphone prediction limiter fail");
         _;
     }
-    function port (uint amount, uint stiphen) public tar(stiphen) returns (bool) {
+    function port (uint amount, uint stiphen) public payble tar(stiphen) returns (bool) {
         // left side, sell
         // cannons return loot
         uint price = amount * sell;
@@ -23,7 +23,7 @@ contract rig {
         return false;
     }
 
-    function star (uint amount, uint stiphen) public tar(stiphen) returns (bool) {
+    function star (uint amount, uint stiphen) public payable tar(stiphen) returns (bool) {
         // right side, buy
         // balls rerun shoot
         uint cost = amount * buy;
