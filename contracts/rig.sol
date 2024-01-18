d91b0d1406ed18d71882a002e4daabf0e8b203c9
David Kamer <me@gitarg.com>
2024-01-18 20:05:56 +0000

    buffer for address

--- contracts/rig.sol
+++ contracts/rig.sol
@@ -1,9 +1,12 @@
 pragma solidity >= "0.8.20";
 import "./gitar.sol";
 
+// use escrow to pay gas for spread of 0.90 - 1.20 (as an example). 
 contract rig {
     uint public sell;
     uint public buy;
+    // REVIEW - struct for transaction, probably not
+    mapping(address => uint) buffer; // homework - what will buffer do?
     constructor (uint _buy, uint _sell, uint booty) {
         sell = _sell;
         buy = _buy;
@@ -16,6 +19,8 @@ contract rig {
         _;
     }
     function port (uint amount, uint stiphen) public payable tar(stiphen) returns (bool) {
+        uint start = gasLeft();
+        uint absorb = start - stiphen;
         // left side, sell
         // cannons return loot
         uint price = amount * sell;
