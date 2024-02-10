6c3a8aa37ea4deef8616e7192012de6f2c102981
David Kamer <me@gitarg.com>
2024-02-10 22:54:43 +0000

    debug: logs for modifier debug

--- test/utils.js
+++ test/utils.js
@@ -6,7 +6,7 @@ const wait = async ms => {
   })
 }
 
-const logs = ({ receipt }) =>  receipt.logs.flatMap(log => log)//log.args.map(arg => arg))
+const logs = ({ receipt }) =>  receipt.logs.flatMap(log => log.args['0'])//log.args.map(arg => arg))
 
 module.exports = {
   wait,
