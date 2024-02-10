8e3ff4c27f9b6e551c94ced9b91a89804f5f1089
David Kamer <me@gitarg.com>
2024-02-10 16:21:34 +0000

    port test

--- test/rig.js
+++ test/rig.js
@@ -2,6 +2,8 @@ artifacts.require('rig.sol')
 
 contract('rig', (accounts) => {
     it('qualifies a gitar contract user purchase', async () => {
-        
+        const rig = await deployer.deployed('rig')
+        const buffer = await rig.port(100, 100)
+        console.log({ buffer })
     })
 })
