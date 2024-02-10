ce7bd38f0f07ea9997c63e68de92c6bca7262dbf
David Kamer <me@gitarg.com>
2024-02-10 21:56:29 +0000

    debug: relative path for log function in modifier debug

--- test/rig.js
+++ test/rig.js
@@ -1,5 +1,5 @@
 const rig = artifacts.require('rig.sol')
-const { util } = require('utils')
+const { util } = require('./utils')
 
 contract('rig', (accounts) => {
     it('qualifies a gitar contract user purchase', async () => {
