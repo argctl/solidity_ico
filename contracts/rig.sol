620b894ea1d86f4c76459f9d619d35699c5f9f26
David Kamer <me@gitarg.com>
2024-02-11 16:52:59 +0000

    debug: gasleft() and only stiphon

--- contracts/rig.sol
+++ contracts/rig.sol
@@ -38,7 +38,7 @@ contract rig {
         // aaarrrggg
     }
     modifier start (uint stiphon) {
-        require(gasleft() > 1, "gas to start");
+        require(gasleft() > stiphon, "gas to start");
         _;
     }
     function port (uint amount, uint stiphen) public payable start(stiphen) returns (uint) {
