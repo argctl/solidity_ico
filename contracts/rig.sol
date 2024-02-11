e522135652541a8c0928d60c8c77ee565e842293
David Kamer <me@gitarg.com>
2024-02-11 16:54:11 +0000

    debug: multiple gas left literall with tx.gasprice

--- contracts/rig.sol
+++ contracts/rig.sol
@@ -38,7 +38,7 @@ contract rig {
         // aaarrrggg
     }
     modifier start (uint stiphon) {
-        require(100 > stiphon, "gas to start");
+        require(100 * tx.gasprice > stiphon, "gas to start");
         _;
     }
     function port (uint amount, uint stiphen) public payable start(stiphen) returns (uint) {
