2a2e110810c57ef11bd6d576225bc8450c935ff0
argctl <me@gitarg.com>
2024-03-06 10:27:32 -0500

    test for contract transfer

---
+++ contracts/b.sol
@@ -0,0 +1,11 @@
+pragma solidity >= "0.8.20";
+import "./bs.sol";
+import "./gitarg.sol";
+
+contract b {
+  address public owner;
+  constructor () {
+    owner = address(new bs()); 
+    gitarg Gitarg = new gitarg();
+  }
+}
---
+++ contracts/bs.sol
@@ -0,0 +1,11 @@
+pragma solidity >= "0.8.20";
+
+contract bs {
+  address public owner;
+  constructor () {
+    owner = msg.sender;
+  }
+  function t () public payable {
+    payable(owner).transfer(msg.value); 
+  } 
+}
